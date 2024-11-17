6c5f3b084b7e94778b77b4c2a04d6c58e664770f
budiyono <budiyono.dev@gmail.com>
2024-11-17 22:39:44 +0700

    migration

---
+++ database/migrations/2024_11_17_115712_create_user_role.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('user_role', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 50)->unique();
+        });
+
+        DB::table('user_role')->insert([
+            ['code'=>'ADMIN'],['code'=>'USER'],['code'=>'WRITER']
+        ]);
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id');
+            $table->boolean('is_member');
+            $table->foreign('role_id')->references('id')->on('user_role');   
+        });
+
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('user_role');
+    }
+};
